54037f2a627e82d6b5d8ba48a29c38be1321c7a2
GilbertDesmond <clark.desmond15@gmail.com>
2017-05-01 12:19:29 +0100

    changes

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+var port = 8000 || process.env.port;
+var mainRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
+var DB = "mongodb://localhost/meanstack";
+var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -5,7 +10,28 @@ var mrogan = require('morgan');
 var path = require('path');
 
 var app = express();
-
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use('/', mainRouter);
+app.use('/api', apiRouter);
+mongoose.connect(DB, function(err){
+    if (err) {
+        return err;
+    }
+
+    else{
+        console.log('Successfully connected to the' + DB);
+    }
+});
+
+app.set('views', __dirname + '/client/views' );
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + 'client'));
+
+app.listen(port, function(){
+    console.log("Listening on port 8000" + port);
+});
+
+
+
